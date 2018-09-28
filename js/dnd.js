4adc0a201ad983169b37a5e43f2f800ec6b8aea3
Krasimir Mladenov <krasimir.mladenov@gmail.com>
2018-09-28 16:21:58 +0300

    test of draggable and droppable element

--- js/dnd.js
+++ js/dnd.js
@@ -1,3 +1,53 @@
-$( function() {
-	$('header').draggable();
+$(function() {
+
+	// [ DRAG ]
+	$('#shop .item').draggable({
+
+		//
+		cursor: "pointer",
+
+		helper: 'clone',
+
+		classes: {
+			"ui-draggable-dragging": "special-draggable"
+		},
+
+		containment: "window",
+
+		revert: "invalid",
+
+		start: function(event, ui) {
+			// Change opacity of static element.
+			$('#inventory').prepend('<div class="slot free temp"></div>');
+			$(this).css('opacity', '0.3');
+
+		},
+
+		stop: function() {
+			// Revert opacity of static element.
+			$(this).css('opacity', '1');
+		}
+	});
+
+	// [ DROP ]
+	$( "#inventory .slot.free" ).droppable({
+		// activate: function() {
+		// 	console.log('activate');
+		// 	$(this).append('<div class="slot new-item"></div>');
+		// },
+		// deactivate: function() {
+
+		// }
+
+		//TODO: Use accept if Hero have enough money for buying item
+		over: function() {
+		// 	$(this).append('<div class="slot new-item"></div>');
+			$(this).parent().css('border', '2px solid #eee');
+		},
+		drop: function(event, ui) {
+
+			$(this).append(ui.draggable);
+		}
+	});
+
 });
